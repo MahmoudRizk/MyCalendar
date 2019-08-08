11b0686164e12f8ba832de5ed76020c92d38a5f4
MahmoudRizk <mahmoudahmedrizk@gmail.com>
2019-08-08 16:12:17 +0200

    Adding text input for calendar events

--- app/components/CalendarEventsList.js
+++ app/components/CalendarEventsList.js
@@ -2,8 +2,12 @@ import React, {Component} from 'react'
 
 import ListGroup from 'react-bootstrap/ListGroup'
 import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
 
 import '../assets/css/CalendarEventsList.css'
+
+import $ from 'jquery';
 
 export class CalendarEventsList extends Component{
 
@@ -11,14 +15,22 @@ export class CalendarEventsList extends Component{
   const view = this.props.result.map((d) => <ListGroup.Item variant="primary">{d.name}</ListGroup.Item>)
 
   const buttonClick = () => {
-    console.log("Key pressed!!!");
-    console.log(this.props.date);
-    this.props.addCalendarEvent("test");
+    const val = $( ".form-control.events-input" ).val().trim();
+    if(val){
+      this.props.addCalendarEvent(val);
+      $( ".form-control.events-input" ).val("");
+    }
+
   }
 
   return(
     <ListGroup className="events-list scrollbar-primary">
-      <Button variant="primary" onClick={buttonClick}>Press Me!</Button>
+      <InputGroup className="mb-3">
+        <InputGroup.Prepend>
+          <Button variant="primary" onClick={buttonClick}>Add</Button>
+        </InputGroup.Prepend>
+        <FormControl aria-describedby="basic-addon1" className="events-input" />
+      </InputGroup>
       {view}
     </ListGroup>
   )}
