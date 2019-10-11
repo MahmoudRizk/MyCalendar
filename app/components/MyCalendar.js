31b56eccd12f32215c13a4d6e0e7c202c19a0a12
mahmoudrizk <mahmoudahmedrizk@gmail.com>
2019-10-11 21:07:59 +0200

    Improving Calendar style

--- app/components/MyCalendar.js
+++ app/components/MyCalendar.js
@@ -6,11 +6,13 @@ export class MyCalendar extends Calendar{
 
   customView = () => {
     // let padDim = $(window).height() / 16
-    $( ".react-calendar__tile" ).css( "padding", "1.4em" );
+    $( ".react-calendar__tile" ).css( "padding", "1.9em" );
     $( ".react-calendar" ).css( "width", "auto" );
     // $( ".react-calendar" ).css( "height", "auto" );
     // console.log($(window).height());
     // console.log($(window).width());
+    $( ".react-calendar" ).css( "border", "0px" );
+    $( ".react-calendar__navigation" ).css( "margin-left", "4%" ).css("margin-right","4%");
   }
 
   componentDidMount(){
